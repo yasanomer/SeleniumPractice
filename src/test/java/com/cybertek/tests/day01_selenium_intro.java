eee734f00b407e62bb38e5e4b511ce9eb38e3fc7
yasanomer <yasanomer@gmail.com>
2020-10-13 15:11:46 -0400

    First practice

---
+++ src/test/java/com/cybertek/tests/day01_selenium_intro.java
@@ -0,0 +1,14 @@
+package com.cybertek.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class day01_selenium_intro {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://facebook.com");
+    }
+}
